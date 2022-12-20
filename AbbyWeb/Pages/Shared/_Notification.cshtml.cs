85c4b4a517d9c5d21f2643f428e307c60c9cbad6
Saydiakhror <botirovs03@gmail.com>
2022-12-21 03:24:00 +0500

    Created CRUD app on Razor Paged

---
+++ AbbyWeb/Pages/Shared/_Notification.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AbbyWeb.Pages.Shared
+{
+    public class _NotificationcshtmlModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
